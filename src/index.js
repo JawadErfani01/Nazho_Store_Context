508bcb36b0d6ac05d9c8a3fc9a43c6c2a8b11585
OmarsSedigh <omarssedig@gmail.com>
2023-01-23 23:23:51 +0430

    home page style changed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
+import "./styles/index.css";
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './styles/index.css';
-import App from './App';
-import React from "react"
-import ReactDOM from "react-dom/client"
-
-import { BrowserRouter } from "react-router-dom"
-import PaginationProvider from './Context/paginationContext';
-import CartProvider from './Context/cartContext';
-import StoreProvider from './Context/storeContext';
-import UserProvider from './Context/userContext';
-const root = ReactDOM.createRoot(document.getElementById("root"))
+import { BrowserRouter } from "react-router-dom";
+import PaginationProvider from "./Context/paginationContext";
+import CartProvider from "./Context/cartContext";
+import StoreProvider from "./Context/storeContext";
+import UserProvider from "./Context/userContext";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-   <UserProvider>
+    <UserProvider>
       <PaginationProvider>
         <CartProvider>
           <StoreProvider>
@@ -20,8 +19,6 @@ root.render(
           </StoreProvider>
         </CartProvider>
       </PaginationProvider>
-      </UserProvider>
+    </UserProvider>
   </BrowserRouter>
-)
-
-
+);
